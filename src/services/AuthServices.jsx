eaa76aaa49b206bf70c5a96510f321ad4116c508
cappycatt <aayushma61aryal@gmail.com>
2026-05-21 16:53:45 +0545

    i lied

--- src/services/AuthServices.jsx
+++ src/services/AuthServices.jsx
@@ -3,7 +3,11 @@ import axios from "axios";
 export const createUser = async (email, password, name) => {
   try {
     console.log(email, password, name);
-    const response = await axios.post("http://localhost:5000/api/auth/signup/", { email, password, name });
+    const response = await axios.post(`${import.meta.env.API_BASEURL}/auth/signup/`, {
+      email,
+      password,
+      name,
+    });
     return response.data;
   } catch (error) {
     console.error("Error registering user:", error);
@@ -13,9 +17,7 @@ export const createUser = async (email, password, name) => {
 
 export const deleteUser = async (id) => {
   try {
-    const response = await axios.delete(
-      `http://localhost:5000/api/auth/signup/${id}`,
-    );
+    const response = await axios.delete(`${import.meta.env.API_BASEURL}/auth/signup/${id}`);
     return response.data;
   } catch (error) {
     console.error("Error while deleting user:", error);
@@ -26,7 +28,7 @@ export const deleteUser = async (id) => {
 export const loginValidation = async (email, password) => {
   try {
     console.log(email, password)
-    const response = await axios.post("http://localhost:5000/api/auth/login/", {
+    const response = await axios.post(`${import.meta.env.API_BASEURL}/auth/login/`, {
       email,
       password,
     });
--- src/services/todoServices.jsx
+++ src/services/todoServices.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
-const API_URL = "http://localhost:5000/api/todos";
-
 // GET all todos
 export const getTodos = async (userId, status) => {
   try {
-    const response = await axios.get(`${API_URL}?userId=${userId}`);
+    const response = await axios.get(`${import.meta.env.API_BASEURL}/todos?userId=${userId}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching todos:", error);
@@ -17,7 +15,7 @@ export const getTodos = async (userId, status) => {
 // POST create todo
 export const createTodo = async (todo, userId) => {
   try {
-    const response = await axios.post(API_URL, { todo, userId });
+    const response = await axios.post(`${import.meta.env.API_BASEURL}/todos`, { todo, userId });
     return response.data;
   } catch (error) {
     console.error("Error creating todo:", error);
@@ -28,7 +26,7 @@ export const createTodo = async (todo, userId) => {
 // PUT update todo
 export const updateTodo = async (id, todo) => {
   try {
-    const response = await axios.put(`${API_URL}/${id}`, { todo });
+    const response = await axios.put(`${import.meta.env.API_BASEURL}/todos/${id}`, { todo });
     return response.data;
   } catch (error) {
     console.error("Error updating todo:", error);
@@ -39,7 +37,7 @@ export const updateTodo = async (id, todo) => {
 // DELETE todo
 export const deleteTodo = async (id) => {
   try {
-    const response = await axios.delete(`${API_URL}/${id}`);
+    const response = await axios.delete(`${import.meta.env.API_BASEURL}/todos/${id}`);
     return response.data;
   } catch (error) {
     console.error("Error deleting todo:", error);
@@ -49,7 +47,7 @@ export const deleteTodo = async (id) => {
 
 export const updateStatus = async (id, status) => {
   try {
-    const response = await axios.put(`${API_URL}/${id}`, { status });
+    const response = await axios.put(`${import.meta.env.API_BASEURL}/todos/${id}`, { status });
     return response.data;
   } catch (error) {
     console.error("Error updating status:", error);
